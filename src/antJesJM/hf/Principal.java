94705112ca1beaa2384e81e27425caad2c98459b
UNO <a.baena@andaluciajunta.es>
2018-01-24 13:21:09 +0100

    creacion de pestañas y tablas. Creacion de botones, no implementados

---
+++ src/antJesJM/hf/Principal.java
@@ -0,0 +1,57 @@
+package antJesJM.hf;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+
+public class Principal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	JTabbedPane pestañas = new JTabbedPane();
+	JPanel actores = new JPanel();
+	JPanel peliculas = new JPanel();
+	JPanel reparto = new JPanel();
+	
+	
+	
+	JButton btnNew = new JButton("Nuevo");
+	JButton btnDel = new JButton("Borrar");
+	JButton btnUpd = new JButton("Modificar");
+	JButton btnSee = new JButton("Ver");
+	String[][] datos = { { "1", "a", "b" }, { "3", "a", "b" }, { "2", "a", "b" }, { "2", "a", "b" }, { "2", "a", "b" },
+			{ "2", "a", "b" }, { "2", "a", "b" }, { "2", "a", "b" }, { "2", "a", "b" } };
+	String[] cabecera = { "id", "Nombre", "Año de Nacimiento" };
+	JTable tabla = new JTable(datos, cabecera);
+	JScrollPane JS = new JScrollPane(tabla);
+
+	public Principal() {
+		setTitle("Hibernate");
+		setLayout(new GridLayout(1,1));
+		JS.setPreferredSize(new Dimension(400, 100));
+
+		actores.add(JS);
+		
+		
+		
+		pestañas.add("actores",actores);
+		pestañas.add("películas", peliculas);
+		pestañas.add("reparto",reparto);
+		add(pestañas);
+
+		setSize(500, 300);
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Principal();
+	}
+
+}
