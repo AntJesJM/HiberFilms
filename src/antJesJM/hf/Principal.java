b7c8587c90cb88a43ccac9e4b745c4ad1a02e25e
Josemi Gonzalez <xemy.013@gmail.com>
2018-01-24 13:54:57 +0100

    Clase para editar visualmente las pestañas creada y comentada por su no completa integracion.

--- src/antJesJM/hf/Principal.java
+++ src/antJesJM/hf/Principal.java
@@ -1,7 +1,12 @@
 package antJesJM.hf;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,6 +14,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.plaf.basic.BasicGraphicsUtils;
+import javax.swing.text.View;
 
 public class Principal extends JFrame {
 
@@ -18,9 +25,7 @@ public class Principal extends JFrame {
 	JPanel actores = new JPanel();
 	JPanel peliculas = new JPanel();
 	JPanel reparto = new JPanel();
-	
-	
-	
+
 	JButton btnNew = new JButton("Nuevo");
 	JButton btnDel = new JButton("Borrar");
 	JButton btnUpd = new JButton("Modificar");
@@ -33,16 +38,15 @@ public class Principal extends JFrame {
 
 	public Principal() {
 		setTitle("Hibernate");
-		setLayout(new GridLayout(1,1));
+		setLayout(new GridLayout(1, 1));
 		JS.setPreferredSize(new Dimension(400, 100));
 
 		actores.add(JS);
-		
-		
-		
-		pestañas.add("actores",actores);
+
+		//pestañas.setUI(new MyTabbedPaneUI());
+		pestañas.add("actores", actores);
 		pestañas.add("películas", peliculas);
-		pestañas.add("reparto",reparto);
+		pestañas.add("reparto", reparto);
 		add(pestañas);
 
 		setSize(500, 300);
@@ -53,5 +57,22 @@ public class Principal extends JFrame {
 	public static void main(String[] args) {
 		new Principal();
 	}
+
+	/*public static class MyTabbedPaneUI extends javax.swing.plaf.basic.BasicTabbedPaneUI {
+
+		@Override
+		protected void paintTab(Graphics g, int tabPlacement, Rectangle[] rects, int tabIndex, Rectangle iconRect,
+				Rectangle textRect) {
+
+			Color savedColor = g.getColor();
+			g.setColor(new Color(185, 9, 11));
+			g.fillRect(rects[tabIndex].x, rects[tabIndex].y, rects[tabIndex].width, rects[tabIndex].height);
+			g.setColor(Color.white);
+			g.drawRect(rects[tabIndex].x, rects[tabIndex].y, rects[tabIndex].width, rects[tabIndex].height);
+			g.setColor(savedColor);
+		}
+	}*/
+
+	
 
 }
