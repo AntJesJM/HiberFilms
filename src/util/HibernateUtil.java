beb35f979c139816b30542409c74ce133832f5a5
Josemi Gonzalez <xemy.013@gmail.com>
2018-02-14 12:28:36 +0100

    Edición del archivo Util de hibernate para crear la conexión

--- src/util/HibernateUtil.java
+++ src/util/HibernateUtil.java
@@ -1,5 +1,37 @@
 package util;
 
-public class HibernateUtil {
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+	private static Session session;
+
+	private static void buildSessionFactory() {
+		try {
+			// Create the SessionFactory from hibernate.cfg.xml
+			sessionFactory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			// Make sure you log the exception, as it might be swallowed
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		buildSessionFactory();
+		return sessionFactory;
+	}
+
+	public static Session getSession() {
+		if (session != null && session.isOpen())
+			return session;
+		else {
+			session = getSessionFactory().openSession();
+			return session;
+		}
+	}
 }
