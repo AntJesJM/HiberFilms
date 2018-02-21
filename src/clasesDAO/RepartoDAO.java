fb3a88feea043d1579bacaffb6dcc1eca8347734
Josemi Gonzalez <xemy.013@gmail.com>
2018-02-21 12:03:57 +0100

    Creación y edicion de la clase RepartoDAO

---
+++ src/clasesDAO/RepartoDAO.java
@@ -0,0 +1,48 @@
+package clasesDAO;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import antJesJM.hf.Principal;
+import clases.Reparto;
+
+public class RepartoDAO {
+
+	public static void guardar(Reparto rep) {
+		Principal.session.beginTransaction();
+		Principal.session.save(rep);
+		Principal.session.getTransaction().commit();
+	}
+
+	public static Reparto buscarPorID(Integer id) {
+		Reparto rep = (Reparto) Principal.session.get(Reparto.class, id);
+		return rep;
+	}
+
+	public static void modificarLoc(Reparto rep) {
+		Principal.session.beginTransaction();
+		Principal.session.update(rep);
+		Principal.session.getTransaction().commit();
+	}
+
+	public static void borrar(Reparto prod) {
+		Principal.session.beginTransaction();
+		Principal.session.delete(prod);
+		Principal.session.getTransaction().commit();
+	}
+
+	public void borrarPorID(int id) {
+		Reparto prod = buscarPorID(id);
+		Principal.session.beginTransaction();
+		Principal.session.delete(prod);
+		Principal.session.getTransaction().commit();
+	}
+
+	public static List<Reparto> buscarTodos() {
+		@SuppressWarnings("unchecked")
+		TypedQuery<Reparto> query = (TypedQuery<Reparto>) Principal.session.createQuery("FROM Reparto");
+		List<Reparto> result = query.getResultList();
+		return result;
+	}
+}
